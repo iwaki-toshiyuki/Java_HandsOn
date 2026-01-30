8c46ddbf9e249bf3e3de2ec6c05d0f50c0366c55
岩木俊幸 <iwakitoshiyuki@iwakitoshiyukinoMacBook-Pro.local>
2026-01-31 02:04:47 +0900

    回文判定問題を追加

---
+++ Palindrome.java
@@ -0,0 +1,17 @@
+public class Palindrome {
+    public static void main(String[] args) {
+            String s = "level";
+
+            boolean isPalindrome = true;
+
+            // 前後から1文字ずつ比較
+            for (int i = 0; i < s.length() / 2; i++) {
+                if (s.charAt(i) != s.charAt(s.length() - 1 - i)) {
+                    isPalindrome = false;
+                    break; // 1つでも違えば終了
+                }
+            }
+
+            System.out.println(isPalindrome ? "YES" : "NO");
+        }
+}
