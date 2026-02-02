6b3b06655ee3627995f3bd72806a7eceb8a4bebf
岩木俊幸 <iwakitoshiyuki@iwakitoshiyukinoMacBook-Pro.local>
2026-02-02 23:02:18 +0900

    偶数の合計問題を追記

---
+++ List_Sum.java
@@ -0,0 +1,17 @@
+// 偶数の合計を計算するJavaプログラム
+public class List_Sum {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        System.out.println("偶数の合計: " + sumEvenNumbers(numbers));
+    }
+    public static int sumEvenNumbers(int[] numbers) {
+        int sum = 0;
+
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] % 2 == 0) {
+                sum += numbers[i];
+            }
+        }
+        return sum;
+        }
+    }
